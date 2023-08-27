4e632c74f81910afe89b3a25c2acb8b9fc688498
Rohan McGovern <rohan@mcgovern.id.au>
2023-08-27 15:25:26 +1000

    Improve timing via fuzzy match of scripts

--- src/background.js
+++ src/background.js
@@ -35,6 +35,131 @@ const ENCODER = new TextEncoder();
 // Mapping from script URL to a timing adjustment (in milliseconds)
 const TIMING_ADJUST = {};
 
+const MS_PER_HOUR = 60 * 60 * 1000;
+const MS_PER_MINUTE = 60 * 1000;
+const MS_PER_SECOND = 1000;
+const MS_PER_CENTISECOND = 10;
+
+
+/**
+ * @param {string} timestr Time string from script e.g. "0:04:08.01"
+ * @returns {Number} timestr converted to milliseconds
+ */
+function script_parse_time(timestr) {
+    let fields = timestr.split(':');
+    let s_cs = fields[2].split('.');
+
+    return (
+        Number(fields[0]) * MS_PER_HOUR
+        + Number(fields[1]) * MS_PER_MINUTE
+        + Number(s_cs[0]) * MS_PER_SECOND
+        + Number(s_cs[1]) * MS_PER_CENTISECOND
+    );
+}
+
+/**
+ * @param {string} text
+ * @returns {string} A normalized form of dialogue text (for comparisons only) 
+ */
+function script_normalize(text) {
+    const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    // Example:
+    // {\i1}...leaving only my footprints behind.{\i0}
+    // =>
+    // leavingonlymyfootprintsbehind
+    let out = '';
+    for (var i = 0; i < text.length; ++i) {
+        let ch = text[i];
+        if (ch == '\\') {
+            ++i;
+            continue;
+        }
+        if (ch == '{') {
+            while (i < text.length && text[i] != '}') {
+                ++i;
+            }
+            continue;
+        }
+        if (CHARS.indexOf(ch) != -1) {
+            out = out + ch;
+        }
+    }
+
+    return out;
+}
+
+/**
+ * Parse and return all Dialogue lines from a script, normalizing the text.
+ * @param {string} script 
+ */
+function script_dialogue_lines(script) {
+    let lines = script.split("\r\n").filter((s) => s.startsWith("Dialogue: "));
+    return lines.map((line) => {
+        let text = script_extract_text(line);
+        let time = script_parse_time(line.split(',', 2)[1]);
+        return { 'raw': line, 'text': script_normalize(text), 'time': time };
+    })
+}
+
+/**
+ * 
+ * @param {string} line 
+ */
+function script_extract_text(line) {
+    // Dialogue: 0,0:00:08.84,0:00:11.30,Default,,0000,0000,0000,,Foo, bar and baz
+    for (var i = 0; i < 9; ++i) {
+        let idx = line.indexOf(',');
+        line = line.substring(idx + 1);
+    }
+    return line;
+}
+
+/**
+ * Try to find a matching line of dialogue between two scripts.
+ * @param {string} script1
+ * @param {string} script2
+ * @return {Array<string>|null} Two matching lines, or null if no match
+ */
+function script_match_text(script1, script2) {
+    let dialog1 = script_dialogue_lines(script1);
+    let dialog2 = script_dialogue_lines(script2);
+    console.debug("dialog example", dialog1);
+
+    for (var i = 0; i < dialog1.length; ++i) {
+        let line1 = dialog1[i];
+        if (line1.text.length < 6) {
+            // Too short for meaningful comparison
+            continue;
+        }
+        for (var j = 0; j < dialog2.length; ++j) {
+            let line2 = dialog2[j];
+            if (line1.text == line2.text) {
+                return [line1, line2];
+            }
+        }
+    }
+    return null;
+}
+
+
+/**
+ * Try to find a timing adjustment which can cause two scripts to (fuzzily) match up.
+ * @param {string} script1
+ * @param {string} script2
+ * @return {number|null} A duration in ms, or null if scripts can't be matched up.
+ */
+function script_timing_adjust(script1, script2) {
+    let matched = script_match_text(script1, script2);
+    console.debug("script match?", matched);
+    if (!matched) {
+        return null;
+    }
+
+    return matched[0].time - matched[1].time;
+}
+
+
 // Wrapper for response from '/play' endpoint
 class PlayResponse {
     constructor(raw) {
@@ -79,13 +204,15 @@ class PlayResponse {
 
     /**
      * Sets the subtitle object of 'lang' to 'obj'.
+     * @param {string} lang Language code e.g. "en-US"
+     * @param {object} obj Subtitle metadata as returned from /play endpoint
      */
     set_subs(lang, obj) {
         this._raw['subtitles'][lang] = obj;
     }
 
     /**
-     * @returns URL of subtitle asset for 'lang', or null.
+     * @returns {string|null} URL of subtitle asset for 'lang', or null.
      */
     sub_asset_url(lang) {
         try {
@@ -208,20 +335,7 @@ class ScriptInterceptor {
      * @param {string} str 
      */
     adjust_timestr(str) {
-        // 0:04:08.01
-        let fields = str.split(':');
-        let s_cs = fields[2].split('.');
-        const MS_PER_HOUR = 60 * 60 * 1000;
-        const MS_PER_MINUTE = 60 * 1000;
-        const MS_PER_SECOND = 1000;
-        const MS_PER_CENTISECOND = 10;
-
-        let original_ms = (
-            Number(fields[0]) * MS_PER_HOUR
-            + Number(fields[1]) * MS_PER_MINUTE
-            + Number(s_cs[0]) * MS_PER_SECOND
-            + Number(s_cs[1]) * MS_PER_CENTISECOND
-        );
+        let original_ms = script_parse_time(str);
         // console.debug("time ms:", str, original_ms);
 
         var new_ms = original_ms + this.adjust;
@@ -326,18 +440,25 @@ class PlayInterceptor {
 
 
         let duration_adjust = await this.duration_adjust(guid, alt_guid);
-        console.debug("should adjust times by", duration_adjust);
 
         console.debug("request obj", this.originalRequest);
         let response = await fetch(altUrl, { "headers": this.headers() });
         let responseJson = await response.json();
         console.debug("Sub response:", responseJson);
 
+        let sub = media._raw.subtitles[DUB_LANG];
         let altsub = responseJson.subtitles[DUB_LANG];
+
+        var script_adjust = null;
+        try {
+            script_adjust = await this.script_adjust(duration_adjust, sub, altsub);
+        } catch (error) {
+            console.warn("Could not adjust times based on script comparison", error);
+        }
 
         if (altsub.format == "ass") {
             let altsub_url = altsub.url;
-            TIMING_ADJUST[altsub_url] = duration_adjust;
+            TIMING_ADJUST[altsub_url] = script_adjust || duration_adjust;
         } else {
             console.warn("Unknown sub format, skipping time adjust", altsub);
         }
@@ -375,6 +496,41 @@ class PlayInterceptor {
         console.debug("duration diff", diff);
 
         return diff;
+    }
+
+    /**
+     * Calculate a timing adjustment by comparing two scripts.
+     *
+     * @param {number} duration_adjust Current adjustment (ms) based on duration.
+     * @param {object} sub Sub object from current media (e.g. en-US)
+     * @param {object} altsub Sub object from alternative media (e.g. ja-JP)
+     * @returns {Promise<number|null>} Suggested adjustment (ms) or null if calculation fails
+     */
+    async script_adjust(duration_adjust, sub, altsub) {
+        if (!sub || !altsub || sub.format != "ass" || altsub.format != "ass") {
+            console.debug("Script-based timing adjustment not available");
+            return null;
+        }
+
+        let subfetch = fetch(sub.url);
+        let altsubfetch = fetch(altsub.url);
+        let script = await (await subfetch).text();
+        let altscript = await (await altsubfetch).text();
+
+        let adjust = script_timing_adjust(script, altscript);
+        if (!adjust) {
+            console.info("Could not calculate any timing adjustment via script comparison");
+            return null;
+        }
+
+        if (Math.abs(adjust - duration_adjust) > 2000) {
+            console.warn(`Script adjustment of ${adjust} too far from duration adjustment of ${duration_adjust}`);
+            return null
+        }
+
+        console.info(`Adjust timing by ${adjust} via fuzzy script comparison`);
+
+        return adjust;
     }
 }
 
